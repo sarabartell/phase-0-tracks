b7f81075fa0c3d24b081ff4c7bfacb93daed026d
Sara Bartell <saracbartell@gmail.com>
2017-06-21 16:42:50 -0700

    Add basic questionare

---
+++ ruby/hamsters.rb
@@ -0,0 +1,25 @@
+##trying to ask user series of hamster questions, end is printing to ruby
+## puts "question"
+## gets.chomp statment
+## if age == "" then puts nil
+## else puts user-input-age
+
+def hamster_questionare
+  puts "What is the hamsters name?"
+  name = gets.chomp
+
+  puts "What is hamsters volume level 1-10"
+  volume = gets.chomp
+
+  puts "What is the fur color?"
+  color = gets.chomp
+
+  puts "Is this hamster a good adoption candidate? Y/N"
+  adoption = gets.chomp
+
+  puts "what is the estimated age of the hamster?"
+  age = gets.chomp
+
+ end
+
+hamster_questionare
