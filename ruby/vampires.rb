ee712b9041041c02e55fe9ed46bbe84a871fd96f
Sara Bartell <saracbartell@gmail.com>
2017-06-23 10:08:47 -0700

    Define vamp age true or false

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -12,3 +12,25 @@ garlic_bread = gets.chomp
 
 p "Would you like to enroll in our health insurance? Y/N"
 insurance = gets.chomp
+
+#birth_day = 2017- year
+# age = gets.chomp
+#how_old = birthday - age
+#if how_old == 0
+#age = true
+#else
+#age = false
+how_old = 2017 - year.to_i
+birth_day = how_old - age.to_i
+
+if birth_day == 0
+  vamp_age = true
+else
+  vamp_age = false
+
+end
+
+
+# if age true where how_old = age == year, AND garlic bread = true OR insurance = true, then not vampire
+
+# if age = false
