8e8a2e2355663464fd41e2bfab4773ac19b9ab0b
Sara Bartell <saracbartell@gmail.com>
2017-06-23 09:26:59 -0700

    Add questions for vampire detection

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+p "What is your name?"
+name = gets.chomp
+
+p "What is your age?"
+age = gets.chomp
+
+p "What year were you born?"
+year = gets.chomp
+
+p "Our cafeteria serves garlic bread. Would you like some? Y/N"
+garlic_bread = gets.chomp
+
+p "Would you like to enroll in our health insurance? Y/N"
+insurance = gets.chomp
