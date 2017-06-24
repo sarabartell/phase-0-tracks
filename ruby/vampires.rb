73a2881f2cc57c4663c0d1b54877b8a8768df9ee
Sara Bartell <saracbartell@gmail.com>
2017-06-24 00:10:20 -0700

    Work on defining variables

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -13,43 +13,41 @@ garlic_bread = gets.chomp
 p "Would you like to enroll in our health insurance? Y/N"
 insurance = gets.chomp
 
-#birth_day = 2017- year
-# age = gets.chomp
-#how_old = birthday - age
-#if how_old == 0
-#age = true
-#else
-#age = false
-how_old = 2017 - year.to_i
-birth_day = how_old - age.to_i
-
-if birth_day == 0
+how_old = 2017 - year
+if age == how_old
   vamp_age = true
 else
   vamp_age = false
 
 end
 
+
 # define garlic_bread
 if garlic_bread == "Y"
-  garlic_okay = true
+  garlic_bread = true
 else
-  garlic_okay = false
+  garlic_bread = false
 
 end
 
-# define insurance
+
+# # define insurance
 if insurance == "Y"
-  insurance_okay = true
+  insurance = true
 else
-  insurance_okay = false
+  insurance = false
 
 end
 
 # if vamp_age = true, AND garlic bread = true OR insurance = true, then p "probably not vampire"
-
+if vamp_age && (insurance_okay || garlic_okay) == true
+  p "probably not vampire"
+else
+  p "invalid"
+end
 
 # if vamp_age = false, AND garlic_bread = false, then p "probably a vampire"
+if vamp_age && (garlic_okay || insurance_okay) == false
 
 # if vamp_age = false, AND garlic_bread = false, AND insurance = false, then p "almost certainly a vampire"
 
