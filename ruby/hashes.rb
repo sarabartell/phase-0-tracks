c287079ff95a10377a06cf83515f90589cbe958a
Sara Bartell <saracbartell@gmail.com>
2017-06-27 16:25:37 -0700

    Add pseudo-code

---
+++ ruby/hashes.rb
@@ -0,0 +1,9 @@
+## want to ask the user for input, fill out the form below:
+# name, address,email, phone, favorite blue,
+#then ask user to select wallpaper
+#then ask user to select ombre
+#put outputs in form of the hash
+#asks for review, if == none, no updates
+## update new strings to symbols (do last maybe)
+# finalize output
+
