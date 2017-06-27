a7bf91175cff5b625cd402d67979f85acb8109f9
Sara Bartell <saracbartell@gmail.com>
2017-06-26 20:21:02 -0700

    Add built in methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -7,33 +7,34 @@
 # "iNvEsTiGaTiOn".<???>
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+puts "zom".insert(2,"o")
 # => “zoom”
 
-# "enhance".<???>
+puts "enhance".center(15)
+
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+puts "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
-#=> "the usual suspects"
-
-# " suspects".<???>
+puts "the usual".insert(9," suspects")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+puts " suspects".insert(0,"the usual")
+# => "the usual suspects"
+
+puts "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+puts "The mystery of the missing first letter"[1..-1]
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+puts "Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
 # "z".<???>
-# => 122 
+# => 122
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+puts "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
