7441aa41d49c20d9d08874fd43890b6f99b4a312
Sara Bartell <saracbartell@gmail.com>
2017-06-28 15:06:01 -0700

    Create an .each and .map! for an array

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,6 +1,32 @@
-def print_animal
-  puts "Welcome to animal lister"
-  yield("Dog", "Cat")
- end
+# def print_animal
+#   puts "Welcome to animal lister"
+#   yield("Dog", "Cat")
+#  end
 
-print_animal { |animal1,animal2| puts "Here is #{animal1}, and #{animal2}" }
+# print_animal { |animal1,animal2| puts "Here is #{animal1}, and #{animal2}" }
+
+animals = [
+  "lion",
+  "deer",
+  "fox"]
+
+animals.each do |animal_type|
+ puts animal_type
+end
+
+puts "original data"
+p animals
+
+animals.map! do |animal|
+  animal.capitalize
+end
+
+puts "after .map"
+p animals
+puts animals
+
+movies_with_animals = {
+  lion: "Lion King",
+  deer: "Bambi",
+  fox: "Fox and the Hound"
+}
