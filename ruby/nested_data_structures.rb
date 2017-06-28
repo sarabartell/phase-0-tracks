136c4f1b042a6302b40759b70ffdbb11d0059522
Sara Bartell <saracbartell@gmail.com>
2017-06-28 11:07:05 -0700

    Add and create structure of gym

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,29 @@
+gym = {
+  first_floor: {
+    rooms:[
+      "Yoga",
+      "Cycling"
+    ]
+    classes:{
+      spin: "10am",
+      vinyasa: "3pm"
+    },
+  },
+  second_floor: {
+    rooms:[
+      "Womens lockers",
+      "Mens lockers"
+    ]
+    classes:[]
+  },
+  third_floor: {
+    rooms:[
+      "Main Studio",
+      "Personal training"
+    ]
+    classes: {
+      TRX: "12pm",
+      body_conditioning: "4pm"
+    },
+  },
+}
