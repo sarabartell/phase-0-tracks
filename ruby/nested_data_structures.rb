98c2f5ef508d93db142ba136a22618e705dbf912
Sara Bartell <saracbartell@gmail.com>
2017-06-28 11:13:28 -0700

    Fix colons and structure of hashes

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,29 +1,30 @@
 gym = {
   first_floor: {
-    rooms:[
-      "Yoga",
-      "Cycling"
-    ]
     classes:{
       spin: "10am",
       vinyasa: "3pm"
     },
+    rooms:[
+      "Yoga",
+      "Cycling"
+    ]
   },
   second_floor: {
+    classes:{
+    },
     rooms:[
       "Womens lockers",
       "Mens lockers"
     ]
-    classes:[]
   },
   third_floor: {
-    rooms:[
-      "Main Studio",
-      "Personal training"
-    ]
     classes: {
       TRX: "12pm",
       body_conditioning: "4pm"
     },
+    rooms:[
+      "Main Studio",
+      "Personal training"
+    ]
   },
 }
