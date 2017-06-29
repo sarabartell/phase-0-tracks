3694524a942dd3fbdb7af297a03fbfbea92ea95e
Sara Bartell <saracbartell@gmail.com>
2017-06-28 17:30:48 -0700

    Create psedou code for challenge

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,12 @@
+# ask user for their name in 2 parts
+# first_name = gets.chomp
+# last_name = gets.chomp
+# want to organize these inputs into an array, once in an array full_name = ['first_name', 'last_name'] , we want to switch the names
+# something like full_name.reverse , so that index 0 and 1 switch the names up
+
+puts "Welcome to name encryptor"
+
+
+# now that names are switched, we want to identify vowels in another array vowels = ['a','e','i','o','u']
+
+# want to swap or replace vowels in full_name with vowels.next or so
