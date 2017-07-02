3cce14e4cb210a3a97e4054b5522d7eb0e92d308
Sara Bartell <saracbartell@gmail.com>
2017-07-02 09:14:33 -0700

    Add pseudo code

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,13 @@
+## define method that takes array of intergers, and interger to search for, outputs the index of that number or return nil if not in array
+
+number_array = [4, 38, 29, 3, 95]
+
+def search_array(array,i)
+index = 0
+puts number_array[index]
+
+
+
+end
+
+search_array(number_array,4)
