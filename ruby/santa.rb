9f7735fd72f744eafc9d7cbcaa68633bd29f9060
Sara Bartell <saracbartell@gmail.com>
2017-07-10 23:05:03 -0700

    Add santa getter and setter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,37 +1,51 @@
 class Santa
 
-def initialize(gender,ethnicity)
- p "Initializing Santa instance..."
- @gender = gender
- @ethnicity = ethnicity
+  def initialize(gender,ethnicity)
+   p "Initializing Santa instance..."
+   @gender = gender
+   @ethnicity = ethnicity
+  end
+
+  def speak
+    "Ho, ho, ho! Haaaapy Holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie)
+    "That was a good #{cookie}!"
+  end
+
+  def get_mad_at(reindeer_name)
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @reindeer_ranking.delete_if {|names| names == reindeer_name}
+    @reindeer_ranking.insert(-1, reindeer_name)
+  end
+
+  def celebrate_birthday(age)
+    @age = age
+    @age += 1
+  end
+
+  def gender=(new_gender)
+    @gender = new_gender
+  end
+
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+
 end
 
-def speak
-  "Ho, ho, ho! Haaaapy Holidays!"
-end
-
-def eat_milk_and_cookies(cookie)
-  "That was a good #{cookie}!"
-end
-
-def get_mad_at(reindeer_name)
-  @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-  @reindeer_ranking.delete_if {|names| names == reindeer_name}
-  @reindeer_ranking.insert(-1, reindeer_name)
-end
-
-def celebrate_birthday(age)
-  @age = age
-  @age += 1
-end
-
-end
-
-holiday = Santa.new("female","white")
-p holiday.speak
-p holiday.eat_milk_and_cookies("chocolate chip")
-p holiday.celebrate_birthday(1)
-p holiday.get_mad_at("Dasher")
+santas = Santa.new("female","white")
+p santas.speak
+p santas.eat_milk_and_cookies("chocolate chip")
+p santas.celebrate_birthday(1)
+p santas.get_mad_at("Dasher")
+p santas.gender = "bisexual"
+puts "Santas age is #{santas.age} and he is #{santas.ethnicity}"
 
 
 santas = []
