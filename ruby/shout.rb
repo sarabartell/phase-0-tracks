050052281f6b42a5133ddb8882ec5de6f16a97e9
Sara Bartell <saracbartell@gmail.com>
2017-07-11 12:06:04 -0700

    Outline module Shout for class Captain and Soldier

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,25 @@
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
 
-  def self.yell_happily(words)
-    words + "!!!" + " :)"
+#   def self.yell_happily(words)
+#     words + "!!!" + " :)"
+#   end
+# end
+
+# puts Shout.yell_angrily("Im mad")
+
+# puts Shout.yell_happily("I'm happpppy")
+
+module Shout
+  def shouting(words)
+    puts words
   end
 end
 
-puts Shout.yell_angrily("Im mad")
+class Captain
+end
 
-puts Shout.yell_happily("I'm happpppy")
+class Soldier
+end
