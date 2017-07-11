6299ce3eece30228f8a7ea9d9b8689b1f9b8ab3e
Sara Bartell <saracbartell@gmail.com>
2017-07-11 11:51:38 -0700

    Add simple should angry and happy methods in module Shout

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,13 @@
 module Shout
-  #info here
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yell_happily(words)
+    words + "!!!" + " :)"
+  end
 end
+
+puts Shout.yell_angrily("Im mad")
+
+puts Shout.yell_happily("I'm happpppy")
