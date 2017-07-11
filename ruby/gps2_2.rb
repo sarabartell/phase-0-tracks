06cb3c353d9cfdd09fbc4f8447957741136dd47c
Sara Bartell <saracbartell@gmail.com>
2017-07-11 16:08:26 -0700

    Add first method and start of calling a new item

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -6,14 +6,40 @@
   # [fill in any steps here]
   # set default quantity
   # print the list to the console [can you use one of your other methods here?]
-# output: print back the hash of the list in a readable way with list item and quantity [what data type goes here, array or hash?]
+# output: print back the hash of the list in a readable way with
+#list item and quantity [what data type goes here, array or hash?]
 
+begin_list = "carrots apples cereal pizza"
+
+
+def create(begin_list)
+  hash_list = {}
+  default_quantity = 1
+  list_array = begin_list.split(" ")
+
+  list_array.each do |item|
+    hash_list[item] = default_quantity
+  end
+
+  p hash_list
+end
+
+first_list = create(begin_list)
 # Method to add an item to a list
 # input: list, item name, and optional quantity
 # steps: take user iput for adding to the list
 # then store the new item in the list with type and quanity if entered, otherwise default to 1
 # output: updated list with new items
 
+def add_item(item,first_list)
+  quantity = 1
+  first_list[item] = quantity
+
+end
+
+add_item("banana",first_list)
+
+p first_list
 # Method to remove an item from the list
 # input: user input
 # steps: take user input and remove called item entirely from the list
