5662f75eb79291ffb5a98efd0ea5084f0cdc1225
Sara Bartell <saracbartell@gmail.com>
2017-07-10 22:12:36 -0700

    Add array input for class Santa and diversity info

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -14,16 +14,26 @@ def eat_milk_and_cookies(cookie)
   "That was a good #{cookie}!"
 end
 
-def my_santa
-  puts @gender
-  puts @ethnicity
-  p @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-  puts @age = 0
-end
+# def my_santa
+#   puts @gender
+#   puts @ethnicity
+#   p @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+#   puts @age = 0
+# end
 
 end
 
-holiday = Santa.new("female","white")
-p holiday.speak
-p holiday.eat_milk_and_cookies("chocolate chip")
-p holiday.my_santa
+# holiday = Santa.new("female","white")
+# p holiday.speak
+# p holiday.eat_milk_and_cookies("chocolate chip")
+# p holiday.my_santa
+
+
+santas = []
+gender = ["female","male","transgender","N/A"]
+ethnicity = ["white","pacific islander","african american","lantio"]
+gender.length.times do |input|
+  santas << Santa.new(gender[input], ethnicity[input])
+end
+
+p santas
