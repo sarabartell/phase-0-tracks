b2b4f3e893c33dc488e528326e5b7913c88bbfd9
Sara Bartell <saracbartell@gmail.com>
2017-07-12 15:57:10 -0700

    Rework methods to take user input for match method, input is guessed letter by user, add guess count and test if include

--- ruby/game_6.7/hangman_game.rb
+++ ruby/game_6.7/hangman_game.rb
@@ -3,20 +3,33 @@ class Hangman
 
   def initialize(user_1_input)
     @secret_array = user_1_input.split("")
+    secret_array = @secret_array
     @is_over = false
     @guess_count = 0
-    p @secret_array
+    @secret_array
    end
 
-  def guessed_word(user_2_input)
-    user_guess = user_2_input.split("")
-    p user_guess
+  def make_word_secret
+    @secret_array.each {|letter| print "- "}
   end
 
+ # def guessed_letter(user_2_input)
+ #    @user_guess = user_2_input
+ #    @user_guess
+ #  end
 
+  def match_guess_to_secret(gussed_letter)
+    @guess_count += 1
+    @user_guess = gussed_letter
+
+    p @secret_array.include?(@user_guess)
+
+  end
 
 end
 
 game = Hangman.new("banana")
 
-game.guessed_word("unicorn")
+game.make_word_secret
+
+game.match_guess_to_secret("f")
