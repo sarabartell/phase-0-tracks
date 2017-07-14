d28a009e638b7531b5a634c1b1125a1467ef7ba6
Sara Bartell <saracbartell@gmail.com>
2017-07-14 10:36:12 -0700

    Start file to practice hangman tests

---
+++ ruby/game_6.7/hangman_test.rb
@@ -0,0 +1,76 @@
+class Hangman
+
+  def initialize(user_1_word)
+    @secret_array = user_1_word.split("")
+    @guessed_letters = []
+    @guess_count = @secret_array.length
+    #6 with banana
+    @blank_word = Array.new(@secret_array.length, "_")
+  end
+
+  def is_over
+    @guess_count == 0
+    #game ends once 6 == 0
+    #add an or statement eventually for if the user_2 word == "banana", once figure out how to make user word
+  end
+
+  def user_2_input(guesses)
+    @guesses = guesses
+  end
+
+  # def guess_already_exsists
+  #   if @guessed_letters.include? @guesses
+  #     puts "you already used #{@guesses}, try again"
+  #     # @guess_count = @guess_count
+  #   end
+  # end
+
+  def guess_is_true
+    if !@guessed_letters.include? @guesses
+       @guessed_letters << @guesses
+      else
+        puts "you already used #{@guesses}, try again"
+        @guess_count += 1
+    end
+    if @secret_array.include? @guesses
+        @guess_is_correct = true
+        p @blank_word
+        @guess_count -= 1
+    else
+        @guess_is_correct = false
+        puts "sorry, #{@guesses} is not in #{@blank_word}, try again"
+        @guess_count -= 1
+    end
+    p @guess_is_correct
+    p @guess_count
+    p @guessed_letters
+    p @guess_count
+  end
+
+  def replace_guesses
+  end
+
+end
+
+game = Hangman.new("banana")
+
+game.user_2_input("a")
+game.guess_is_true
+
+
+game.user_2_input("x")
+game.guess_is_true
+
+
+game.user_2_input("a")
+game.guess_is_true
+
+
+game.user_2_input("a")
+game.guess_is_true
+
+game.user_2_input("c")
+game.guess_is_true
+
+game.user_2_input("b")
+game.guess_is_truegit
