cc2d535d5d7c43864dd0e2da7a393b4a30994a75
Sara Bartell <saracbartell@gmail.com>
2017-07-15 16:05:17 -0700

    Create test for instance variable/user 2 guesses

--- ruby/game_6.7/hangman_game_spec.rb
+++ ruby/game_6.7/hangman_game_spec.rb
@@ -1,2 +1,10 @@
 require_relative 'hangman_game'
 
+describe Hangman do
+  let(:hangman_game) {Hangman.new("banana")}
+
+  it "user 2 input is equal to instance variable @guesses" do
+    expect(hangman_game.user_2_input("h")).to eq "h"
+  end
+
+end
