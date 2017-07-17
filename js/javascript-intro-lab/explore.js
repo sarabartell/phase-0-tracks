5759694b10bcb636162f8fe6f6e8f3426668973a
Sara Bartell <saracbartell@gmail.com>
2017-07-17 11:22:33 -0700

    Add reverse java method

--- js/javascript-intro-lab/explore.js
+++ js/javascript-intro-lab/explore.js
@@ -1,3 +1,14 @@
 // step 1: take a word into a function as an argument/parameter
 // step 2: go through the string, and move each letter to its reverse spot, example, "hello" "h" goes from first letter to last letter, but certain letters like "l" will appear to not move even though they will
+// want to go through the length - 1
 // output the new string value reversed
+
+function reverse(string) {
+  var reversed = "";
+  for (var i = string.length - 1; i >= 0; i -= 1){
+    reversed += string[i];
+  }
+  return reversed;
+}
+
+console.log(reverse("hello"));
