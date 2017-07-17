01d03756fbe52f2b80d055334a4268aed3c65f94
Sara Bartell <saracbartell@gmail.com>
2017-07-17 11:05:44 -0700

    add pseudocode

---
+++ js/javascript-intro-lab/explore.js
@@ -0,0 +1,3 @@
+// step 1: take a word into a function as an argument/parameter
+// step 2: go through the string, and move each letter to its reverse spot, example, "hello" "h" goes from first letter to last letter, but certain letters like "l" will appear to not move even though they will
+// output the new string value reversed
