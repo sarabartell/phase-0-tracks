b045082edf3e7d9755dbab51cc487fb7e6ff145c
Sara Bartell <saracbartell@gmail.com>
2017-07-18 15:21:30 -0700

    Write pseudo code for first release

---
+++ js/algos.js
@@ -0,0 +1,9 @@
+var longestPhrase = ["Sara", "Sara is my name","Sara = me", "I am Sara"]
+
+// write a method that finds the longest phrase
+//loop through each, during each loop find the phrase at that index, print that and find the phrase length
+  // if somehow phrase(1) is longest, print phrase at 1
+
+function findLongest(longestPhrase){
+
+}
