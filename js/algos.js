7db1edfe75b6b61589c7e7c56fffece9286d8c5a
Sara Bartell <saracbartell@gmail.com>
2017-07-20 18:49:40 -0700

    start to add comparative/find function

--- js/algos.js
+++ js/algos.js
@@ -1,23 +1,41 @@
-//var longestPhrase = ["Sara", "Sara is my name","Sara = me", "I am Sara!"]
 
+
+// funtion code
 // write a method that finds the longest phrase
 //loop through each, during each loop find the phrase at that index, print that and find the phrase length
   // if somehow phrase(1) is longest, print phrase at 1
 
-function findLongest(longestPhrase){
-  var largerThan = 0;
+// function findLongest(longestPhrase){
+//   var largerThan = 0;
 
-  for(var i = 0; i < longestPhrase.length; i++){
-      if (longestPhrase[i].length > largerThan){
-      largerThan = longestPhrase[i].length;
-      word = longestPhrase[i];
+//   for(var i = 0; i < longestPhrase.length; i++){
+//       if (longestPhrase[i].length > largerThan){
+//       largerThan = longestPhrase[i].length;
+//       word = longestPhrase[i];
+//     }
+//   }
+//   return word;
+// }
+
+
+function findMatches(hash1,hash2){
+  for (var hash1[value]){
+    if (hash1[value] == hash2[value]){
+      true;
+    }
+    else{
+      false;
     }
   }
-  return word;
 }
 
-console.log(findLongest(["Sara", "Banana","Peanutbutter", "pizza"]));
+words({name: "Sara", age: 24}, {name: "Sammy", age: 24});
+words({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3});
 
-console.log(findLongest(["beans","limezzz","edamame","cauliflowers"]));
+// return info code
+// console.log(findLongest(["Sara", "Banana","Peanutbutter", "pizza"]));
 
-console.log(findLongest(["water","skinny soy latte","t","tea"]));
+// console.log(findLongest(["beans","limezzz","edamame","cauliflowers"]));
+
+// console.log(findLongest(["water","skinny soy latte","t","tea"]));
+
