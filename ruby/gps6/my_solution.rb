2613a2577a0b734cfed2d6fdbbf7140ce90b51aa
Sara Bartell <saracbartell@gmail.com>
2017-07-20 17:07:19 -0700

    Refactor to run method for every state

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -76,20 +76,21 @@ end
 #need: state name, pop. density, and popultaion
 #will have to call virus effects as well on each state
 STATE_DATA.each do |state_name, state_info|
-
+  state = VirusPredictor.new(state_name, state_info[:population_density], state_info[:population])
+  state.virus_effects
 end
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
+# alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+# alabama.virus_effects
 
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
+# jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
+# jersey.virus_effects
 
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
+# california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+# california.virus_effects
 
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
+# alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
+# alaska.virus_effects
 
 
 #=======================================================================
