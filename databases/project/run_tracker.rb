5ef651cceb4649c7870c6b3c5539ef1dd859bf95
Sara Bartell <saracbartell@gmail.com>
2017-07-26 16:09:23 -0700

    Create methods to add and remove runs

--- databases/project/run_tracker.rb
+++ databases/project/run_tracker.rb
@@ -1,6 +1,7 @@
 require 'sqlite3'
 
 runs = SQLite3::Database.new("runs.db")
+runs.results_as_hash = true
 
 create_runs_table = <<-SQL
     CREATE TABLE IF NOT EXISTS runs(
@@ -13,3 +14,15 @@ SQL
 
 runs.execute(create_runs_table)
 # runs.execute("INSERT INTO runs (length,time,day) VALUES (3.2,'00:40:05','2/4/2017')")
+
+def add_run(runs, length, time, day)
+runs.execute("INSERT INTO runs (length, time, day) VALUES (?, ?, ?)", [length, time, day])
+end
+
+def remove_run(runs,day)
+  runs.execute("DELETE FROM runs WHERE day=(?)", [day])
+end
+
+# add_run(runs, 4, '00:35:05','7/26/2017')
+# remove_run(runs,'7/26/2017')
+#UI
