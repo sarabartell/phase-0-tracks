db51259beaba84eeefe38c40eaf34d80ea716077
Sara Bartell <saracbartell@gmail.com>
2017-07-26 14:44:49 -0700

    Set up insert proper time and dates

--- databases/project/run_tracker.rb
+++ databases/project/run_tracker.rb
@@ -5,10 +5,11 @@ runs = SQLite3::Database.new("runs.db")
 create_runs_table = <<-SQL
     CREATE TABLE IF NOT EXISTS runs(
     id INTEGER PRIMARY KEY,
-    length INT,
-    time INT,
-    day INT
+    length DECIMAL(10,2),
+    time TIME,
+    day VARCHAR(255)
     )
 SQL
 
 runs.execute(create_runs_table)
+# runs.execute("INSERT INTO runs (length,time,day) VALUES (3.2,'00:40:05','2/4/2017')")
